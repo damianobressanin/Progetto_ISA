6b00803cfcb9845d6908488dfb1b7b164350e9ad
Damiano Bressanin <damiano.bressanin@edu.unife.it>
2024-01-14 23:03:12 +0100

    Modifica visibilita' metodi

--- isaCalcAgri/src/main/java/it/isa/progetto/Prodotto.java
+++ isaCalcAgri/src/main/java/it/isa/progetto/Prodotto.java
@@ -15,71 +15,71 @@ public class Prodotto {
         this.IVA = IVA;
     }
 
-    public static Prodotto creaPiantaLavanda() {
+    static Prodotto creaPiantaLavanda() {
         return new Prodotto("Pianta lavanda", 8.50, TipoProdotto.AGRICOLO, 10.0);
     }
 
-    public static Prodotto creaPiantaSalvia() {
+    static Prodotto creaPiantaSalvia() {
         return new Prodotto("Pianta salvia", 5.0, TipoProdotto.AGRICOLO, 5.0);
     }
 
-    public static Prodotto creaPiantaRosmarino() {
+    static Prodotto creaPiantaRosmarino() {
         return new Prodotto("Pianta rosmarino", 4.5, TipoProdotto.AGRICOLO, 5.0);
     }
 
-    public static Prodotto creaMazzo() {
+    static Prodotto creaMazzo() {
         return new Prodotto("Mazzo", 8.0, TipoProdotto.AGRICOLO, 4.0);
     }
 
-    public static Prodotto creaSacchettino() {
+    static Prodotto creaSacchettino() {
         return new Prodotto("Sacchettino", 5.0, TipoProdotto.AGRICOLO, 4.0);
     }
 
-    public static Prodotto creaGufetto() {
+    static Prodotto creaGufetto() {
         return new Prodotto("Gufetto", 10.0, TipoProdotto.AGRICOLO, 22.0);
     }
 
-    public static Prodotto creaCuore() {
+    static Prodotto creaCuore() {
         return new Prodotto("Cuore", 8.0, TipoProdotto.AGRICOLO, 22.0);
     }
 
-    public static Prodotto creaSottoCuscino() {
+    static Prodotto creaSottoCuscino() {
         return new Prodotto("Sotto cuscino", 6.0, TipoProdotto.AGRICOLO, 4.0);
     }
 
-    public static Prodotto creaCremaMani() {
+    static Prodotto creaCremaMani() {
         return new Prodotto("Crema mani", 8.90, TipoProdotto.COMMERCIALE, 22.0);
     }
 
-    public static Prodotto creaBagnodoccia() {
+    static Prodotto creaBagnodoccia() {
         return new Prodotto("Bagnodoccia", 7.90, TipoProdotto.COMMERCIALE, 22.0);
     }
 
-    public static Prodotto creaSaponetta() {
+    static Prodotto creaSaponetta() {
         return new Prodotto("Saponetta", 4.90, TipoProdotto.COMMERCIALE, 22.0);
     }
 
-    public static Prodotto creaOlioAngustifolia() {
+    static Prodotto creaOlioAngustifolia() {
         return new Prodotto("Olio essenziale angustifolia", 11.0, TipoProdotto.TRASFORMAZIONE, 22.0);
     }
 
-    public static Prodotto creaOlioIbrida() {
+    static Prodotto creaOlioIbrida() {
         return new Prodotto("Olio essenziale ibrida", 11.0, TipoProdotto.TRASFORMAZIONE, 22.0);
     }
 
-    public String getNome() {
+    String getNome() {
         return nome;
     }
 
-    public double getPrezzo() {
+    double getPrezzo() {
         return prezzo;
     }
 
-    public TipoProdotto getTipo() {
+    TipoProdotto getTipo() {
         return tipo;
     }
 
-    public double getIVA() {
+    double getIVA() {
         return IVA;
     }
 
