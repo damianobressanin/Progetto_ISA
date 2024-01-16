702e3e4fa03796031f4cf7448038285ece5380a0
Damiano Bressanin <damiano.bressanin@edu.unife.it>
2024-01-16 12:52:22 +0100

    Aggiunta di Vendita e dei suoi test

---
+++ isaCalcAgri/src/main/java/it/isa/progetto/Vendita.java
@@ -0,0 +1,148 @@
+package it.isa.progetto;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Date;
+
+public class Vendita {
+    private Map<Prodotto, Integer> carrello;
+    private Inventario inventario;
+    private RegistroVendite registroVendite;
+
+    public Vendita(Inventario inventario, RegistroVendite registroVendite) {
+        this.carrello = new HashMap<>();
+        this.inventario = inventario;
+        this.registroVendite = registroVendite;
+    }
+
+    boolean aggiungiProdottoCarrello(Prodotto prodotto, int quantita) {
+        if (quantita <= 0) {
+            return false;
+        }
+        // Controllo disponibilità.
+        // Se l'utente vuole una quantità maggiore di quella disponibile nell'inventario
+        // allora aggiungi tutta la quantità disponibile nel carrello.
+        int quantitaCorrente = carrello.getOrDefault(prodotto, 0);
+        if (inventario.controllaDisponibilita(prodotto, (quantita + quantitaCorrente)) == false) {
+            int quantitaDisponibile = inventario.getQuantitaDisponibile(prodotto);
+            if (quantitaDisponibile == 0) {
+                return false;
+            } else {
+                carrello.put(prodotto, quantitaDisponibile);
+                return true;
+            }
+
+        }
+        // Aggiorno il carrello
+        carrello.put(prodotto, quantitaCorrente + quantita);
+        return true;
+    }
+
+    int getQuantitaProdotto(Prodotto prodotto) {
+        return carrello.getOrDefault(prodotto, 0);
+    }
+
+    void azzeraProdotto(Prodotto prodotto) {
+        carrello.put(prodotto, 0);
+    }
+
+    boolean rimuoviProdotto(Prodotto prodotto, int quantita) {
+        // se l'utente inserisce un numero negativo
+        // oppure non c'è il prodotto sul carrello
+        // allora non modificare la quantità
+        int quantitaCarrello = getQuantitaProdotto(prodotto);
+        if (quantita > 0 && quantitaCarrello > 0) {
+            // se la quantità nel carrello è maggiore o uguale alla quantità da rimuovere
+            // allora sottraila
+            if (quantitaCarrello >= quantita) {
+                carrello.put(prodotto, quantitaCarrello - quantita);
+            } else {
+                // altrimenti azzera
+                azzeraProdotto(prodotto);
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    String riepilogoVendita() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Totale: ").append(calcolaTotale()).append("\n\n");
+        sb.append("Totale scontrino immediato: ")
+                .append(calcolaTotaleCommerciale() + calcolaTotaleTrasformazione()).append("\n\n");
+        sb.append("Totale Agricolo: ").append(calcolaTotaleAgricolo()).append("\n");
+        sb.append("Totale Commerciale: ").append(calcolaTotaleCommerciale()).append("\n");
+        sb.append("Totale Trasformazione: ").append(calcolaTotaleTrasformazione()).append("\n\n");
+        sb.append("Contenuto del carrello: \n").append(visualizzaCarrello()).append("\n");
+        return sb.toString();
+    }
+
+    double calcolaTotale() {
+        double totale = 0.0;
+        for (Map.Entry<Prodotto, Integer> entry : carrello.entrySet()) {
+            totale += entry.getKey().getPrezzo() * entry.getValue();
+        }
+        return totale;
+    }
+
+    private double calcolaTotaleAgricolo() {
+        double totale = 0.0;
+        for (Map.Entry<Prodotto, Integer> entry : carrello.entrySet()) {
+            if (entry.getKey().getTipo() == TipoProdotto.AGRICOLO) {
+                totale += entry.getKey().getPrezzo() * entry.getValue();
+            }
+        }
+        return totale;
+    }
+
+    private double calcolaTotaleCommerciale() {
+        double totale = 0.0;
+        for (Map.Entry<Prodotto, Integer> entry : carrello.entrySet()) {
+            if (entry.getKey().getTipo() == TipoProdotto.COMMERCIALE) {
+                totale += entry.getKey().getPrezzo() * entry.getValue();
+            }
+        }
+        return totale;
+    }
+
+    private double calcolaTotaleTrasformazione() {
+        double totale = 0.0;
+        for (Map.Entry<Prodotto, Integer> entry : carrello.entrySet()) {
+            if (entry.getKey().getTipo() == TipoProdotto.TRASFORMAZIONE) {
+                totale += entry.getKey().getPrezzo() * entry.getValue();
+            }
+        }
+        return totale;
+    }
+
+    String visualizzaCarrello() {
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<Prodotto, Integer> entry : carrello.entrySet()) {
+            sb.append(entry.getKey().getNome());
+            sb.append(" - Quantità: ");
+            sb.append(entry.getValue());
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    void svuotaCarrello() {
+        carrello.clear();
+    }
+
+    void finalizzaVendita() {
+        // aggiungo al registroVendite solo i prodotti agricoli venduti
+        for (Map.Entry<Prodotto, Integer> entry : carrello.entrySet()) {
+            if (entry.getKey().getTipo() == TipoProdotto.AGRICOLO) {
+                registroVendite.registraVendita(new Date(), entry.getKey(), entry.getValue());
+            }
+        }
+        // Rimuovo dall'inventario tutti i prodotti acquistati dal cliente
+        for (Map.Entry<Prodotto, Integer> entry : carrello.entrySet()) {
+            inventario.rimuoviProdotto(entry.getKey(), entry.getValue());
+        }
+        // Svuota il carrello dopo aver completato la vendita
+        svuotaCarrello();
+    }
+}
---
+++ isaCalcAgri/src/test/java/it/isa/progetto/VenditaTest.java
@@ -0,0 +1,215 @@
+package it.isa.progetto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class VenditaTest {
+    private Vendita vendita;
+    private Inventario inventario;
+    private RegistroVendite registroVendite;
+    private static Prodotto prodotto1;
+    private static Prodotto prodotto2;
+    private static Prodotto prodotto3;
+    private static Prodotto prodotto4;
+    private static Prodotto prodotto5;
+
+    @BeforeAll
+    static void setUpProdotti() {
+        // i prodotti hanno i campi final, quindi li metto nel BeforeAll
+        prodotto1 = Prodotto.creaMazzo();
+        prodotto2 = Prodotto.creaPiantaSalvia();
+        prodotto3 = Prodotto.creaPiantaLavanda();
+        prodotto4 = Prodotto.creaCremaMani();
+        prodotto5 = Prodotto.creaOlioAngustifolia();
+    }
+
+    @BeforeEach
+    void setUp() {
+        inventario = new Inventario();
+        inventario.aggiungiProdotto(prodotto1, 10);
+        inventario.aggiungiProdotto(prodotto2, 10);
+        inventario.aggiungiProdotto(prodotto3, 10);
+        inventario.aggiungiProdotto(prodotto4, 10);
+        inventario.aggiungiProdotto(prodotto5, 10);
+
+        registroVendite = new RegistroVendite();
+
+        vendita = new Vendita(inventario, registroVendite);
+    }
+
+    @Test
+    void testAggiungiProdottoCarrello() {
+        assertTrue(vendita.aggiungiProdottoCarrello(prodotto1, 5));
+        assertEquals(5, vendita.getQuantitaProdotto(prodotto1));
+    }
+
+    @Test
+    void testAggiungiProdottoCarrelloGiaEsistente() {
+        assertTrue(vendita.aggiungiProdottoCarrello(prodotto1, 2));
+        assertTrue(vendita.aggiungiProdottoCarrello(prodotto1, 7));
+
+        assertEquals(9, vendita.getQuantitaProdotto(prodotto1));
+    }
+
+    @Test
+    void testAggiungiTantiProdottiCarrello() {
+        assertTrue(vendita.aggiungiProdottoCarrello(prodotto1, 1));
+        assertTrue(vendita.aggiungiProdottoCarrello(prodotto2, 3));
+        assertTrue(vendita.aggiungiProdottoCarrello(prodotto3, 8));
+        assertTrue(vendita.aggiungiProdottoCarrello(prodotto4, 7));
+        assertTrue(vendita.aggiungiProdottoCarrello(prodotto5, 5));
+
+        assertTrue(vendita.aggiungiProdottoCarrello(prodotto1, 5));
+        assertTrue(vendita.aggiungiProdottoCarrello(prodotto2, 1));
+
+        assertEquals(6, vendita.getQuantitaProdotto(prodotto1));
+        assertEquals(4, vendita.getQuantitaProdotto(prodotto2));
+        assertEquals(8, vendita.getQuantitaProdotto(prodotto3));
+        assertEquals(7, vendita.getQuantitaProdotto(prodotto4));
+        assertEquals(5, vendita.getQuantitaProdotto(prodotto5));
+    }
+
+    @Test
+    void testAggiungiProdottoCarrelloQuantitaNegativa() {
+        assertFalse(vendita.aggiungiProdottoCarrello(prodotto1, -5));
+        assertEquals(0, vendita.getQuantitaProdotto(prodotto1));
+    }
+
+    @Test
+    void testAggiungiProdottoCarrelloQuantitaZero() {
+        assertFalse(vendita.aggiungiProdottoCarrello(prodotto1, 0));
+        assertEquals(0, vendita.getQuantitaProdotto(prodotto1));
+    }
+
+    @Test
+    void testAggiungiProdottoCarrelloQuantitaDisponibile() {
+        int quantitaDisponibile = inventario.getQuantitaDisponibile(prodotto1);
+
+        assertTrue(vendita.aggiungiProdottoCarrello(prodotto1, 10));
+        assertEquals(quantitaDisponibile, vendita.getQuantitaProdotto(prodotto1));
+    }
+
+    @Test
+    void testAggiungiProdottoCarrelloTroppaQuantita() {
+        int quantitaDisponibile = inventario.getQuantitaDisponibile(prodotto1);
+
+        assertTrue(vendita.aggiungiProdottoCarrello(prodotto1, quantitaDisponibile * 2));
+        assertEquals(quantitaDisponibile, vendita.getQuantitaProdotto(prodotto1));
+    }
+
+    @Test
+    void testRimuoviProdotto() {
+        vendita.aggiungiProdottoCarrello(prodotto1, 7);
+
+        assertTrue(vendita.rimuoviProdotto(prodotto1, 2));
+        assertEquals(5, vendita.getQuantitaProdotto(prodotto1));
+    }
+
+    @Test
+    void testRimuoviStessoProdotto() {
+        vendita.aggiungiProdottoCarrello(prodotto1, 7);
+
+        assertTrue(vendita.rimuoviProdotto(prodotto1, 2));
+        assertTrue(vendita.rimuoviProdotto(prodotto1, 2));
+        assertEquals(3, vendita.getQuantitaProdotto(prodotto1));
+    }
+
+    @Test
+    void testRimuoviTantiProdotti() {
+        vendita.aggiungiProdottoCarrello(prodotto1, 7);
+        vendita.aggiungiProdottoCarrello(prodotto2, 3);
+        vendita.aggiungiProdottoCarrello(prodotto3, 9);
+        vendita.aggiungiProdottoCarrello(prodotto4, 4);
+        vendita.aggiungiProdottoCarrello(prodotto5, 5);
+
+        assertTrue(vendita.rimuoviProdotto(prodotto1, 5));
+        assertTrue(vendita.rimuoviProdotto(prodotto2, 2));
+
+        assertEquals(2, vendita.getQuantitaProdotto(prodotto1));
+        assertEquals(1, vendita.getQuantitaProdotto(prodotto2));
+        assertEquals(9, vendita.getQuantitaProdotto(prodotto3));
+        assertEquals(4, vendita.getQuantitaProdotto(prodotto4));
+        assertEquals(5, vendita.getQuantitaProdotto(prodotto5));
+    }
+
+    @Test
+    void testRimuoviProdottoQuantitaNegativa() {
+        vendita.aggiungiProdottoCarrello(prodotto1, 7);
+
+        assertFalse(vendita.rimuoviProdotto(prodotto1, -2));
+        assertEquals(7, vendita.getQuantitaProdotto(prodotto1));
+    }
+
+    @Test
+    void testRimuoviProdottoQuantitaZero() {
+        vendita.aggiungiProdottoCarrello(prodotto1, 5);
+
+        assertFalse(vendita.rimuoviProdotto(prodotto1, 0));
+        assertEquals(5, vendita.getQuantitaProdotto(prodotto1));
+    }
+
+    @Test
+    void testRimuoviProdottoQuantitaCarrello() {
+        vendita.aggiungiProdottoCarrello(prodotto1, 5);
+
+        assertTrue(vendita.rimuoviProdotto(prodotto1, 5));
+        assertEquals(0, vendita.getQuantitaProdotto(prodotto1));
+    }
+
+    @Test
+    void testRimuoviTroppoProdotto() {
+        vendita.aggiungiProdottoCarrello(prodotto1, 5);
+
+        assertTrue(vendita.rimuoviProdotto(prodotto1, 100));
+        assertEquals(0, vendita.getQuantitaProdotto(prodotto1));
+    }
+
+    @Test
+    void testRimuoviProdottoNonNelCarrello() {
+        assertFalse(vendita.rimuoviProdotto(prodotto1, 5));
+        assertEquals(0, vendita.getQuantitaProdotto(prodotto1));
+    }
+
+    @Test
+    void testTotale() {
+        vendita.aggiungiProdottoCarrello(prodotto1, 5);
+        vendita.aggiungiProdottoCarrello(prodotto2, 3);
+        vendita.aggiungiProdottoCarrello(prodotto3, 9);
+        vendita.aggiungiProdottoCarrello(prodotto4, 4);
+        vendita.aggiungiProdottoCarrello(prodotto5, 0);
+
+        double totale = vendita.getQuantitaProdotto(prodotto1) * prodotto1.getPrezzo()
+                + vendita.getQuantitaProdotto(prodotto2) * prodotto2.getPrezzo()
+                + vendita.getQuantitaProdotto(prodotto3) * prodotto3.getPrezzo()
+                + vendita.getQuantitaProdotto(prodotto4) * prodotto4.getPrezzo()
+                + vendita.getQuantitaProdotto(prodotto5) * prodotto5.getPrezzo();
+        assertEquals(totale, vendita.calcolaTotale());
+    }
+
+    @Test
+    void testTotaleCarrelloVuoto() {
+        assertEquals(0, vendita.calcolaTotale());
+    }
+
+    @Test
+    void testFinalizzaVendita() {
+        vendita.aggiungiProdottoCarrello(prodotto1, 5);
+        vendita.aggiungiProdottoCarrello(prodotto1, 1);
+        vendita.aggiungiProdottoCarrello(prodotto2, -3);
+        vendita.aggiungiProdottoCarrello(prodotto3, 11);
+        vendita.aggiungiProdottoCarrello(prodotto4, 4);
+        vendita.rimuoviProdotto(prodotto4, 1);
+        vendita.aggiungiProdottoCarrello(prodotto5, 0);
+
+        vendita.finalizzaVendita();
+
+        assertEquals(4, inventario.getQuantitaDisponibile(prodotto1));
+        assertEquals(10, inventario.getQuantitaDisponibile(prodotto2));
+        assertEquals(0, inventario.getQuantitaDisponibile(prodotto3));
+        assertEquals(7, inventario.getQuantitaDisponibile(prodotto4));
+        assertEquals(10, inventario.getQuantitaDisponibile(prodotto5));
+    }
+}
